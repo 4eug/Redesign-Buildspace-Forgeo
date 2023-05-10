09fdeb7387bc42d7d00cc4b60b39c0db2055f90d
4eug <debraheug@gmail.com>
2023-05-10 14:57:32 +0000

    Bookmark Icon

---
+++ src/components/icons/bookmark.tsx
@@ -0,0 +1,24 @@
+export const Bookmark = ({ className }: { className?: string }) => (
+  <svg
+    width="17"
+    height="21"
+    viewBox="0 0 17 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5.04224 7.71741H11.8972"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M15.9192 7.961C15.7612 1.807 14.4142 1 8.47024 1C2.08324 1 1.00424 1.932 1.00424 9.429C1.00424 17.822 0.847243 20 2.44324 20C4.03824 20 6.64324 16.316 8.47024 16.316C10.2972 16.316 12.9022 20 14.4972 20C15.8532 20 15.9442 18.428 15.9402 12.811"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
