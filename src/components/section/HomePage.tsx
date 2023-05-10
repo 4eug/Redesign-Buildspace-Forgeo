c7b520be8e55cc75ed34c8d09449af32ac18a197
4eug <debraheug@gmail.com>
2023-05-10 18:39:19 +0000

    Update with Tablet Cards

--- src/components/section/HomePage.tsx
+++ src/components/section/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Hero, HeroSubtitle, HeroTitle } from '../hero';
 import { Button } from '../button';
-import CardLine1 from './CardLine1';
-import CardLine2 from './CardLine2';
+import CardLine1 from './Desktop Cards/CardLine1';
+import CardLine2 from './Desktop Cards/CardLine2';
+import CardLine3 from './Tablet Cards/CardLine3';
+import CardLine4 from './Tablet Cards/CardLine4';
 
 export default function HomeHero() {
   return (
-    <div className="flex items-center">
+    <div className="grid">
       <Hero>
         <div className="lg:mt-8">
           <HeroTitle>
@@ -38,11 +40,20 @@ export default function HomeHero() {
           </div>
         </div>
       </Hero>
+      {/* Desktop Cards */}
       <div className="absolute top-0 lg:left-[60%] lg:block hidden w-full">
         <CardLine1 />
       </div>
       <div className="absolute top-0 lg:left-[78%] lg:block hidden w-full">
         <CardLine2 />
+      </div>
+
+      {/* Tablet Cards */}
+      <div className="relative mt-10 right-[20%] lg:hidden md:block hidden">
+        <CardLine3 />
+      </div>
+      <div className="absolute top-[800px] right-[-21%] lg:hidden md:block hidden">
+        <CardLine4 />
       </div>
     </div>
   );
