aff32471e21985812f86fdb3d8e0f41a3fcd6bcb
4eug <debraheug@gmail.com>
2023-05-10 14:58:00 +0000

    CardLines Desktop

---
+++ src/components/section/CardLine1.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardLg from '../cards/cardlg';
+import { Cardline1 } from '@/data/data';
+
+export default function CardLine1() {
+  return (
+    <div className="marquee-container">
+      <div className="marquee1 grid gap-6">
+        {Cardline1.map((item, index) => (
+          <CardLg key={index} imageSrc={item.image} text={item.text} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/section/CardLine2.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardLg from '../cards/cardlg';
+import { Cardline2 } from '@/data/data';
+
+export default function CardLine2() {
+  return (
+    <div className="marquee-container">
+      <div className="marquee2 grid gap-6">
+        {Cardline2.map((item, index) => (
+          <CardLg key={index} imageSrc={item.image} text={item.text} />
+        ))}
+      </div>
+    </div>
+  );
+}
