070a232b8e39fd12a2492f44bfe2507999c8052b
4eug <debraheug@gmail.com>
2023-05-10 14:57:19 +0000

    Data for Image Rendering

---
+++ src/data/data.ts
@@ -0,0 +1,45 @@
+export const Cardline1 = [
+  {
+    image: '/nft-redlady.png',
+    text: 'I want to have a fashion brand',
+  },
+  {
+    image: '/nft-girl.png',
+    text: 'I want to upload my original song',
+  },
+  {
+    image: '/nft-greenboy.png',
+    text: 'I want to have an NFT project farm',
+  },
+  {
+    image: '/nft-techgirl.png',
+    text: 'I want to have an AI PA',
+  },
+  {
+    image: '/nft-girl.png',
+    text: 'I want to upload my original song',
+  },
+];
+
+export const Cardline2 = [
+  {
+    image: '/nft-manga.png',
+    text: 'I want to publish a manga',
+  },
+  {
+    image: '/nft-jedi.png',
+    text: 'I want to produce an animated film',
+  },
+  {
+    image: '/nft-boy.png',
+    text: 'i want to build a website',
+  },
+  {
+    image: '/nft-robotgirl.png',
+    text: 'Interested in designing and building robots',
+  },
+  {
+    image: '/nft-girl.png',
+    text: 'I want to upload my original song',
+  },
+];
